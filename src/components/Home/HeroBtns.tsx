c78973bfcd06edf106dc22f230de314e3d2fbcd7
iamBijoyKar <bijoypoco12345@gmail.com>
2023-12-03 16:06:53 +0530

    [FIX]: hero btns fixed

--- src/components/Home/HeroBtns.tsx
+++ src/components/Home/HeroBtns.tsx
@@ -1,20 +1,21 @@
 "use client";
 import { FaAngleRight } from "react-icons/fa";
 import { BsPeopleFill } from "react-icons/bs";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function HeroBtns() {
+  const router = useRouter();
   return (
     <div className="flex justify-center items-center mt-6">
       <button
-        onClick={() => redirect("/")}
+        onClick={() => router.push("/")}
         className="flex justify-center items-center bg-sky-500 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded-full"
       >
         <span className="mr-2">Get Started</span>
         <FaAngleRight />
       </button>
       <button
-        onClick={() => redirect("/people")}
+        onClick={() => router.push("/people")}
         className="flex justify-center items-center bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-4 rounded-full ml-4"
       >
         <span className="mr-2">View People</span>
