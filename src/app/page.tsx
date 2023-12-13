30be7ec217eba1b8161da3a97282ce7dae275a3b
iamBijoyKar <bijoypoco12345@gmail.com>
2023-12-13 17:05:05 +0530

    [UPDATE]: hero UI updated, logo added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,42 @@ import HeroBtns from "@/components/Home/HeroBtns";
 import { BsGithub } from "react-icons/bs";
 import { MdOutlineMoneyOff } from "react-icons/md";
 import { MdSecurity } from "react-icons/md";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <section className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-6xl font-bold ">BioDrop</h1>
-        <p className="mt-3 text-2xl">A simple app for sharing your bio links</p>
-        <div className="flex gap-2 mt-4 font-medium">
+      <section className="flex flex-col items-center justify-start min-h-[90vh] p-2 my-10 sm:mt-0">
+        <Image
+          quality={100}
+          src="/bio-drop.png"
+          alt="Hero Image"
+          width={300}
+          height={300}
+          className="-mt-4"
+        />
+        <h1 className="text-6xl font-bold -mt-4">BioDrop</h1>
+        <p className="mt-3 text-2xl text-center">
+          A simple app for sharing your bio links
+        </p>
+        <div className="flex gap-2 mt-4 font-medium w-full justify-center">
           <span className="flex gap-2 items-center">
             <BsGithub className="text-xl" />
-            Open Source
+            <span className="">Open Source</span>
           </span>
           |
           <span className="flex gap-2 items-center">
-            Free Forever
             <MdOutlineMoneyOff className="text-xl " />
+            <span className="">
+              Free <span className="hidden sm:inline-block">forever</span>
+            </span>
           </span>
           |
           <span className="flex gap-2 items-center">
-            No Signup Require
             <MdSecurity className="text-xl " />
+            <span className="">
+              No Signup <span className="hidden sm:inline-block">required</span>
+            </span>
           </span>
         </div>
         <HeroBtns />
